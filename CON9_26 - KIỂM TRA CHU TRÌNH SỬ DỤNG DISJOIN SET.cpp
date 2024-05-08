4f6b51f798cdccfe8ed0e60db657a058569dfadc
Pham Thanh <102653760+mrbrownnn@users.noreply.github.com>
2024-05-08 09:57:58 +0700

    Add files via upload

--- CON9_26 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cpp
+++ CON9_26 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cpp
@@ -1,89 +1,58 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
-
-class DisjointSet {
-private:
-    vector<int> parent, rank;
-
-public:
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n);
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
+int n, m, x, y, ok;
+vector<int> svs;
+vector<bool> check;
+vector<vector<int>> a;
+void BFS(int k)
+{
+    check[k] = 0;
+    for (auto i : a[k])
+    {
+        if (check[i])
+        {
+            svs[i] = k;
+            BFS(i);
         }
-    }
-
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    void unionSets(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX == rootY) {
+        else if (i != svs[k])
+        {
+            ok = 1;
             return;
         }
-
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else {
-            parent[rootY] = rootX;
-            rank[rootX]++;
-        }
     }
-};
-
-bool hasCycle(int V, vector<pair<int, int>>& edges) {
-    DisjointSet ds(V);
-
-    for (auto edge : edges) {
-        int u = edge.first;
-        int v = edge.second;
-
-        int rootU = ds.find(u);
-        int rootV = ds.find(v);
-
-        if (rootU == rootV) {
-            return true;
-        }
-
-        ds.unionSets(rootU, rootV);
-    }
-
-    return false;
 }
-
-int main() {
-    int T;
-    cin >> T;
-
-    while (T--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<pair<int, int>> edges(E);
-
-        for (int i = 0; i < E; i++) {
-            cin >> edges[i].first >> edges[i].second;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m;
+        ok = 0;
+        a.clear();
+        svs.clear();
+        check.clear();
+        a.resize(n + 1);
+        svs.resize(n + 1, -1);
+        check.resize(n + 1, 1);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> x >> y;
+            a[x].push_back(y);
+            a[y].push_back(x);
         }
-
-        if (hasCycle(V, edges)) {
+        for (int i = 1; i <= n; i++)
+            if (check[i] && !ok)
+                BFS(i);
+        if (ok)
             cout << "YES" << endl;
-        } else {
+        else
             cout << "NO" << endl;
-        }
     }
-
-    return 0;
 }
+
+
+//B21DCVT404//
