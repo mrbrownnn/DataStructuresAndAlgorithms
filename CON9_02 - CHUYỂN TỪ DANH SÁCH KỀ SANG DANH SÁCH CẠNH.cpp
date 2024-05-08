0a6dfeb7e4840f6b6c200ab2dc9143feb14fbb0d
Pham Thanh <102653760+mrbrownnn@users.noreply.github.com>
2024-05-08 10:30:22 +0700

    Add files via upload

--- CON9_02 - CHUYỂN TỪ DANH SÁCH KỀ SANG DANH SÁCH CẠNH.cpp
+++ CON9_02 - CHUYỂN TỪ DANH SÁCH KỀ SANG DANH SÁCH CẠNH.cpp
@@ -1,40 +1,27 @@
-#include <bits/stdc++.h>
-#define endl "\n"
-#define fi first
-#define se second
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define mod 1000000007
+
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	string s;
-	set<pair<int, int>> m;
-	cin.ignore();
-	for (int i = 0; i < n; i++)
-	{
-		getline(cin, s);
-		s += ' ';
-		int k = 0;
-		for (int j = 0; j < s.size(); j++)
-		{
-			if (s[j] >= '0' && s[j] <= '9')
-			{
-				k = k * 10 + s[j] - '0';
-			}
-			else
-			{
-				int a = i + 1, b = k;
-				if (a > b)
-					swap(a, b);
-				m.insert(make_pair(a, b));
-				k = 0;
-			}
-		}
-	}
-	for (auto i : m)
-	{
-		cout << i.fi << " " << i.se << endl;
-	}
+   int n;
+   cin>>n;
+   vector<pair<int,int>>v;
+   int x;
+   char c;
+   for(int i=1;i<=n;i++){
+    while(scanf("%d%c",&x,&c)!=EOF){
+        if(x>i){
+        v.push_back({i,x});
+        }
+        if(c=='\n'){
+            break;
+        }
+    }
+   }
+   for(auto it:v){
+    cout<<it.first<<" "<<it.second<<endl;
+   }
+    return 0;
 }
