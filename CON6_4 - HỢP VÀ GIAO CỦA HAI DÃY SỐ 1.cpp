2c5e6c7e5aecdbb61fbc27b3212d08a633cffa0e
Pham Thanh <102653760+mrbrownnn@users.noreply.github.com>
2024-05-26 18:41:59 +0700

    Add files via upload

--- CON6_4 - HỢP VÀ GIAO CỦA HAI DÃY SỐ 1.cpp
+++ CON6_4 - HỢP VÀ GIAO CỦA HAI DÃY SỐ 1.cpp
@@ -1,69 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void faster(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-}
-
-void nhap(vector<long long> &a){
-    vector<long long>:: iterator it;
-    for(it = a.begin();it != a.end();it++){
-        cin >> *it;
-    }
-}
-
-void xu_ly(vector<long long> a,vector<long long> b){
-    set<long long> uni;
-    vector<long long> inter;
-    vector<long long> res;
-    vector<long long>:: iterator it;
-    for(it = a.begin();it != a.end();it++){
-        res.push_back(*it);
-    }
-    for(it = b.begin();it != b.end();it++){
-        res.push_back(*it);
-    }  
-
-    sort(res.begin(),res.end());
-
-// tìm và in phần tử mảng giao nhau
-    for(int i = 0;i < res.size();i++){
-        uni.insert(res.at(i));
-    }
-    set<long long>:: iterator un;
-    for(un = uni.begin();un != uni.end();un++){
-        cout << *un << " ";
-    }
-    cout << endl;
-// tìm và in phần tử mảng hợp nhau
-    for(int i = 0;i < res.size();i++){
-        if(res[i] == res[i+1]){
-            inter.push_back(res[i]);
-        }
-    }
-    for(it = inter.begin();it != inter.end();it++){
-        cout << *it << " ";
-    }
-}
-
-void solution(){
-    faster();
-    int t;
-    cin >> t;
-    while(t--){
-        long long n,m;
-        cin >> n >> m;
-        vector<long long> a(n);
-        vector<long long> b(m);
-        nhap(a);
-        nhap(b);
-        xu_ly(a,b);
-        cout << endl;
-    }
-}
 
 int main(){
-    solution();
-    return 0;
+
+	
+	int t; cin >> t;
+	while(t--){
+		int n, m; cin >> n >> m;
+		vector<int> a(n);
+		set<int> st, sm;
+		for(int i = 0; i < n; i ++){
+			cin >> a[i];
+			st.insert(a[i]);
+		}
+		int k = 0;
+		for(int i = 1; i <= m; i ++){
+			int x; cin >> x;
+			st.insert(x);
+			if(n + i - st.size() - k != 0){
+				sm.insert(x);
+				k++;
+			}
+		}
+		for(auto x: st) cout << x << " ";
+		cout << endl;
+		for(auto x: sm) cout << x << " ";
+		cout << endl;
+	}
+	return 0;
 }
